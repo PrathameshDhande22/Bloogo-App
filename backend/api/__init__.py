5b86d8db5cbbf7b78bc24f372ac3e7aaf5ed84ae
Prathameshdhande22 <prathameshdhande534@gmail.com>
2023-08-10 17:28:00 +0530

    CORS origin changes

--- backend/api/__init__.py
+++ backend/api/__init__.py
@@ -41,7 +41,7 @@ def create_app() -> FastAPI:
 
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=[os.getenv("APP_URL"), os.getenv("SUBDOMAIN_URL")],
+        allow_origins=["https://bloogo.vercel.app", "https://vercel.app"],
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
